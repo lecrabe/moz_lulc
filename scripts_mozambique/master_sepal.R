3f76b6048419542cd1ea1709f201e24bfadb03b2
remi.dannunzio@gmail.com <remi.dannunzio@gmail.com>
2017-11-03 00:03:15 +0000

    V0 operational decision tree

--- scripts_mozambique/master_sepal.R
+++ scripts_mozambique/master_sepal.R
@@ -14,7 +14,9 @@ scriptdir   <- "~/moz_lulc/scripts_mozambique/"
 #############################################################
 ### SETUP PARAMETERS
 #############################################################
-source(paste0(scriptdir,"s0_setup_parameters_and_folders.R"),echo=TRUE)
+source(paste0(scriptdir,"s0_load_packages_set_folders.R"),echo=TRUE)
+source(paste0(scriptdir,"my_parameters.R"),echo=TRUE)
+
 
 #############################################################
 ### DOWNLOAD AND CLIP GLOBAL PRODUCTS
@@ -24,39 +26,27 @@ source(paste0(scriptdir,"s2_global_data/download_gfc_2016.R"),echo=TRUE)
 source(paste0(scriptdir,"s2_global_data/clip_LC_products.R"),echo=TRUE)
 
 
-
 #############################################################
 ### GET CLASSIFICATION PERFORMED IN SEPAL AND MERGE TILES
 #############################################################
-
-### VARIABLES TO MODIFY
-downclassdir <- paste0(rootdir,"downloads/classif_zambezia_lsat_train_520_poly_wetperiod/")
-classif_name <- "classification_20171101.tif"
-
 source(paste0(scriptdir,"s3_classification/merge_classification.R"),echo=TRUE)
 
 
 #############################################################
 ### CREATE SEGMENTS OVER THE AOI AND INTEGRATE VALUES
 #############################################################
-
-### VARIABLES TO MODIFY
-downmosaidir <- paste0(rootdir,"downloads/zambezia_lsat_dry_20171102/")
-mosaic_name  <- "lsat_2016_zambezia.tif"
-
 source(paste0(scriptdir,"s4_segments/merge_mosaic.R"),echo=TRUE)
 source(paste0(scriptdir,"s4_segments/segmentation.R"),echo=TRUE)
-source(paste0(scriptdir,"s4_segments/decision_tree.R"),echo=TRUE)
 
 
 #############################################################
 ### COMBINE PRODUCTS IN A DECISION TREE
 #############################################################
-source(paste0(scriptdir,"s5_decision_tree/TBD_XXXX.R"),echo=TRUE)
-time_decision_tree <- Sys.time() - time_start
+source(paste0(scriptdir,"s5_decision_tree/decision_tree.R"),echo=TRUE)
+
 
 #############################################################
 ### USE AD GRID (Collect Earth exercise) TO ASSESS ACCURACY
 #############################################################
-source(paste0(scriptdir,"s6_accuracy_assessment/aa_esa_map.R"),echo=TRUE)
-time_AA <- Sys.time() - time_start
+#source(paste0(scriptdir,"s6_accuracy_assessment/aa_map_with_ad_grid.R"),echo=TRUE)
+
